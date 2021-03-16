5e2b001cb9354a2e871134c23d3c7237cbdb9097
Benjamin Godlove <ben@pluslabs.org>
2021-03-16 14:28:47 -0400

    Reproduction of expo-in-app-purchases Invariant Violation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as InAppPurchases from "expo-in-app-purchases";
 
 export default function App() {
   return (
